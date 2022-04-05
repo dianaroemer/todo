fcd46671d39252e9add5d6d9fdbcc64ea96005f3
dominicroemer <dominicroemer@gmail.com>
2022-04-05 16:40:43 -0700

    Cleaned code somewhat, changed to mouseenter, for more consistent functionality. Still somewhat janky. Transitions unfinished

--- src/utils/dropDownMenu.js
+++ src/utils/dropDownMenu.js
@@ -18,21 +18,13 @@ Parameters:
 */
 function dropDownMenu(parentNode, direction) {
 
-    console.log('I am in dropDownMenu!');
-
-
-
-
-
     const dropDownContainer = document.createElement('menu');
-    dropDownContainer.classList.add('drop-down-container-hidden');
+    dropDownContainer.classList.add('drop-down-container');
     dropDownContainer.style.position = "absolute";
     dropDownContainer.style.listStyle = "none";
     dropDownContainer.style.height = "max-content";
     dropDownContainer.style.width = "max-content";
     dropDownContainer.style.opacity = "0.1"
-
-
     dropDownContainer.style.padding = "5px 5px 5px 5px"
     dropDownContainer.style.background = "rgba(0,0,0, .1)";
     dropDownContainer.style.display = "none";
@@ -42,34 +34,32 @@ function dropDownMenu(parentNode, direction) {
     // dropDownContainer.style.transform = "translateX(-25%)"
     dropDownContainer.style.transition = "opacity 0.3s";
 
-
     if(direction === `down`) {
         // dropDownContainer.style.left = "-22px";
         dropDownContainer.style.flexDirection = "column"
         // dropDownContainer.style.bottom = "20px;"
-    } /* else if(direction === `up`) {
-        dropDownContainer.style.left = "-22px";
-        dropDownContainer.style.top = "-112px"
+    }  else if(direction === `up`) {
+        dropDownContainer.style.transform = "translateY(-120%)";
+        // dropDownContainer.style.left = "-22px";
+        // dropDownContainer.style.top = "-112px"
         dropDownContainer.style.flexDirection = "column-reverse"
-        dropDownContainer.style.gridTemplateColumns
     } else if(direction === `right`) {
         // const newOffset = Number(parentNode.style.width);
         // dropDownContainer.style.left = `${newOffset}px`;       
-
         // dropDownContainer.style.top = "-112px"
         dropDownContainer.style.flexDirection = "row"
     } else if(direction === `left`) {
-        const newOffset = Number(parentNode.style.width);
-        dropDownContainer.style.left = `${newOffset}px`;
-        dropDownContainer.style.top = "-112px"
+        // const newOffset = Number(parentNode.style.width);
+        // dropDownContainer.style.left = `${newOffset}px`;
+        // dropDownContainer.style.top = "-112px"
         dropDownContainer.style.flexDirection = "row-reverse"
     } 
-    */
+    
 
     const [, , ...dropDownList] = arguments;
 
     dropDownList.forEach( element => {
-        console.log(element);
+        // console.log(element);
         const newDropDown = document.createElement('li');
         newDropDown.innerText = element;
         newDropDown.style.width = "max-content"
@@ -78,18 +68,14 @@ function dropDownMenu(parentNode, direction) {
     });
     
 
-
-
     // Attach Event Listeners for hover or click
     // dropDownContainer.onmouseover = () => console.log(`shit`);
-    parentNode.addEventListener('mouseover', (e) => {
+    parentNode.addEventListener('mouseenter', (e) => {
 
-        console.log(e.currentTarget);
+        // console.log(e.currentTarget);
 
-        if(e.target !== e.currentTarget){
-            //  console.log("child hovered") 
-        } else {
-       
+        // This specific if statement ensures the following code doesn't trigger on children activation
+        if(e.target === e.currentTarget){
             // console.log('hover');
 
             if(dropDownContainer.style.display === "none") {
@@ -111,8 +97,6 @@ function dropDownMenu(parentNode, direction) {
 
         }
     })
-
-
 
 
     // Attach finished dropDownContainer to parent object
